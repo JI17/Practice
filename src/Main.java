2ec02a3dd1d64a5fd4a030fc6eb450d07c7e3f35
JI17 <jonnyice17@gmail.com>
2018-04-24 12:32:47 +0300

    Добавление одного java-файла, изменение файла Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Update file");
+        Numb n = new Numb();
+        n.setI(10);
+        n.sum(5);
+        System.out.println(n.getI());
     }
+
 }
---
+++ src/Numb.java
@@ -0,0 +1,16 @@
+public class Numb {
+    private int i = 0;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void sum(int i1) {
+        this.i += i1;
+    }
+
+}
