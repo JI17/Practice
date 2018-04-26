4f7552435b9560b4b0bd4be424fad5087790c907
JI17 <jonnyice17@gmail.com>
2018-04-26 19:46:42 +0300

    Обновление класса StreamAPI

--- src/StreamAPI.java
+++ src/StreamAPI.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamAPI {
     static List<String> mList = Arrays.asList("1aa1", "dd1", "1bb2", "aa3", "cc3");
@@ -10,5 +12,17 @@ public class StreamAPI {
                 .map(String::toUpperCase)
                 .sorted()
                 .forEach(System.out::println);
+
+        IntStream.range(1, 10)
+                .forEach(System.out::println);//вывод значений 1-9 без цикла
+
+        Stream.of("1aa1", "dd1", "1bb2", "aa3", "cc3")//создание потока без создания коллекции
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        IntStream.range(2, 16)
+                .mapToObj(i -> "c" + i)
+                .forEach(System.out::println);
+
     }
 }
